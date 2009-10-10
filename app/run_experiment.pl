bf369194e008c1be03321160dbbd78fbc965680a
J. J. Merelo <jj@merelo.net>
2009-10-10 09:52:18 +0000

    reverted run_experiment

--- app/run_experiment.pl
+++ app/run_experiment.pl
@@ -8,7 +8,8 @@ use lib qw(../lib ../../Algorithm-Evolutionary/lib);
 use YAML qw(LoadFile);
 use IO::YAML;
 use DateTime;
-use Data::Dumper qw(Dumper);
+
+use Algorithm::MasterMind qw( check_combination );
 
 my $config_file = shift || die "Usage: $0 <configfile.yaml>\n";
 
@@ -17,35 +18,33 @@ $config_file .= ".yaml" if ! ($config_file =~ /\.ya?ml$/);
 my $conf = LoadFile($config_file) || die "Can't open $config_file: $@\n";
 
 my $method = $conf->{'Method'};
-eval {
-  require( "Algorithm::MasterMind::$method");
-}|| die "Can't load $method: $@\n";
+eval "require Algorithm::MasterMind::$method" || die "Can't load $method: $@\n";
 
 my $io = IO::YAML->new($conf->{'ID'}."-$method-".DateTime->now().".yaml", ">");
 my $method_options = $conf->{'Method_options'};
 
 for my $i (1..$conf->{'tests'}) {
-  
+
   my $solver;
-  eval "\$solver = new Algorithm::MasterMind::$method ".Dumper($method_options);
-  die "Can't instantiate $solver: $@\n";
+  eval "\$solver = new Algorithm::MasterMind::$method \$method_options";
+  die "Can't instantiate $solver: $@\n" if !$solver;
   my $secret_code = $solver->random_combination();
-  $io->print( $secret_code );
+  my $game = { code => $secret_code,
+	       combinations => []};
   my $first_string = $solver->issue_first;
   my $response =  check_combination( $secret_code, $first_string);
-  $io->print( [$first_string,$response] );
+  push @{$game->{'combinations'}}, [$first_string,$response] ;
 
   $solver->feedback( $response );
     
   my $played_string = $solver->issue_next;
   while ( $played_string ne $secret_code ) {
-      $response = check_combination( $secret_code, $played_string);
-      $io->print( [$played_string, $response] );
-      $solver->feedback( $response );
-      print p(print_combination($played_string, $response )), "\n";
-      $played_string = $solver->issue_next;      
-    }  
-  $io->print( combinations => $solver->evaluated() );
-  $io->terminate;
+    $response = check_combination( $secret_code, $played_string);
+    push @{$game->{'combinations'}}, [$played_string, $response] ;
+    $solver->feedback( $response );
+    $played_string = $solver->issue_next;      
+  }  
+  $game->{'evaluations'} = $solver->evaluated();
+  $io->print($game);
 }
 $io->close;
