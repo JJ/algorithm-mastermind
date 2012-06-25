4635986bd83a51bd0af4de688ec5f06f281263b0
J. J. Merelo <jj@merelo.net>
2012-06-25 16:52:17 +0000

    Wrap up

--- app/IEEE-CIG/plot-csets-plus.R
+++ app/IEEE-CIG/plot-csets-plus.R
@@ -1,7 +1,6 @@
 plot(tapply(X=(cset.most.4.6$Played), INDEX=list(cset.most.4.6$Evaluations), FUN=mean),type="l", lty='dashed',
      ylab='Average size',xlab='Moves',
      main='Remaining combinations',
-     col='red',
-     sub='histogram',log='y')
+     col='red', log='y')
 lines(tapply(X=(cset.plus.4.6$Played), INDEX=list(cset.plus.4.6$Evaluations), FUN=mean),col='green',lty='dotted')
 lines(tapply(X=(cset.entropy.4.6$Played), INDEX=list(cset.entropy.4.6$Evaluations), FUN=mean),col='black')
--- app/IEEE-CIG/plot-top-plus-4-8.R
+++ app/IEEE-CIG/plot-top-plus-4-8.R
@@ -3,4 +3,5 @@ plot(tapply(X=(cset.in.most1.4.8$IsIn), INDEX=list(cset.in.most1.4.8$Move), FUN=
      main='Probability secret code in top scorers, l=4, c=8',
      col='red')
 lines(tapply(X=(cset.in.plus.4.8$IsIn), INDEX=list(cset.in.plus.4.8$Move), FUN=mean),col='blue',lty='dotted')
+lines(tapply(X=(cset.in.plus2.4.8$IsIn), INDEX=list(cset.in.plus2.4.8$Move), FUN=mean),col='brown',lty='dotdash')
 lines(tapply(X=(cset.in.entropy1.4.8$IsIn), INDEX=list(cset.in.entropy1.4.8$Move), FUN=mean),col='black')
