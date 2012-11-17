413032abce87bfe29ffa05495a55654f3657329e
JJ Merelo <jjmerelo@gmail.com>
2012-11-17 19:33:06 +0100

    Adapted to Player

--- app/run_experiment_instances.pl
+++ app/run_experiment_instances.pl
@@ -9,12 +9,11 @@ use YAML qw(LoadFile);
 use File::Slurp qw(read_file);
 use IO::YAML;
 use DateTime;
-use Algorithm::Combinatorics qw(variations_with_repetition);
 
-use Algorithm::MasterMind qw( check_combination );
+use Algorithm::MasterMind::Player qw( play );
 
 my $config_file = shift || die "Usage: $0 <configfile.yaml>\n";
-my $instances_file = shift || "instancias4_8.txt";
+my $instances_file = shift || "../experiments/instances/instancias4_8.txt";
 
 $config_file .= ".yaml" if ! ($config_file =~ /\.ya?ml$/);
 
@@ -36,41 +35,7 @@ my @combinaciones = split( /\n/, $instances );
 
 while ( my $combination = shift @combinaciones ) {
   my $secret_code = join("",map( $alphabet[$_-1], split(/\s+/, $combination)));
-  my $solver;
-  eval "\$solver = new Algorithm::MasterMind::$method \$method_options";
-  die "Can't instantiate $solver: $@\n" if !$solver;
-  print "Code $secret_code\n";
-  my $game = { code => $secret_code,
-	       combinations => []};
-  my $move = $solver->issue_first;
-  my $response =  check_combination( $secret_code, $move);
-  push @{$game->{'combinations'}}, [$move,$response] ;
-
-  while ( $move ne $secret_code ) {
-    $solver->feedback( $response );
-    $move = $solver->issue_next;
-    print "Playing $move\n";
-    $response = check_combination( $secret_code, $move);
-    push @{$game->{'combinations'}}, [$move, $response] ;
-    $solver->feedback( $response );
-    if ( $solver->{'_consistent'} ) {
-      push @{$game->{'consistent_set'}}, [ keys %{$solver->{'_consistent'}} ] ;
-    }  else {
-      my $partitions = $solver->{'_partitions'};
-      push @{$game->{'consistent_set'}}, 
-	[ map( $_->{'_string'}, @{$partitions->{'_combinations'}}) ];
-      if ( $partitions->{'_score'}->{'_most'} ) {
-	push @{$game->{'top_scorers'}}, [ $partitions->top_scorers('most') ];
-      } elsif ( $partitions->{'_score'}->{'_entropy'} ) {
-	push @{$game->{'top_scorers'}}, [ $partitions->top_scorers('entropy') ];
-      }
-    }
-    if ( $solver->{'_data'} ) {
-      push @{$game->{'data'}}, $solver->{'_data'};
-    }
-  }
-  
-  $game->{'evaluations'} = $solver->evaluated();
+  my $game = play( $secret_code,  $method, $method_options );
   $io->print($game);
   print "Finished\n";
 }
